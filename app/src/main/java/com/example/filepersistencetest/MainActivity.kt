b762ff146660ca88c19f15321fe29749fa20b757
Rachel-oc <mengting.xie@thoughtworks.com>
2023-04-12 17:33:42 +0800

    refactor

--- app/src/main/java/com/example/filepersistencetest/MainActivity.kt
+++ app/src/main/java/com/example/filepersistencetest/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.filepersistencetest
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -19,7 +17,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.core.content.contentValuesOf
 import com.example.filepersistencetest.ui.theme.FilePersistenceTestTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -37,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(viewModel = viewModel, this)
+                    Greeting(viewModel = viewModel)
                 }
             }
         }
@@ -51,13 +48,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(viewModel: MainViewModel, context: Context) {
+fun Greeting(viewModel: MainViewModel) {
     val text by viewModel.text.collectAsState()
     val count by viewModel.count.collectAsState(initial = 0)
     val protoCount by viewModel.protoCount.collectAsState(initial = 0)
     val scope = rememberCoroutineScope()
-    val myDatabaseHelper = MyDatabaseHelper(context, "BookStore.db", 3)
-    val db = myDatabaseHelper.writableDatabase
     Column {
         TextField(
             value = text,
@@ -70,58 +65,22 @@ fun Greeting(viewModel: MainViewModel, context: Context) {
         Button(onClick = viewModel::loadBySharedPreferences) {
             Text(text = "show data")
         }
-        Button(onClick = {
-            val values = contentValuesOf(
-                "name" to "The Da Vinci Code",
-                "author" to "Dan Brown",
-                "pages" to 454,
-                "price" to 16.96
-            )
-            db.insert("Book", null, values)
-        }) {
+        Button(onClick = viewModel::createDatabase){
+            Text(text = "create")
+        }
+        Button(onClick = viewModel::insert) {
             Text(text = "add data")
         }
-        Button(onClick = {
-            db.delete("Book", "pages < ?", arrayOf("500"))
-        }) {
+        Button(onClick = viewModel::delete) {
             Text(text = "delete data")
         }
-        Button(onClick = {
-            val values = contentValuesOf("price" to 10.99)
-            db.update("Book", values, "name = ?", arrayOf("The Da Vinci Code"))
-        }) {
+        Button(onClick = viewModel::update) {
             Text(text = "update data")
         }
-        Button(onClick = {
-            val cursor = db.query("Book", null, null, null, null, null, null)
-            if (cursor.moveToFirst()) {
-                do {
-                    val name = cursor.getString(cursor.getColumnIndexOrThrow("name"))
-                    Log.d("DataBase", "book name is $name")
-                } while (cursor.moveToNext())
-            }
-            cursor.close()
-        }) {
+        Button(onClick = viewModel::query) {
             Text(text = "query data")
         }
-        Button(onClick = {
-            db.beginTransaction()
-            try {
-                db.delete("Book", null, null)
-                val values = contentValuesOf(
-                    "name" to "Game of Thrones",
-                    "author" to "George Martin",
-                    "pages" to 720,
-                    "price" to 20.85
-                )
-                db.insert("Book", null, values)
-                db.setTransactionSuccessful()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            } finally {
-                db.endTransaction()
-            }
-        }) {
+        Button(onClick = viewModel::transaction) {
             Text(text = "replace data")
         }
         Button(onClick = {
@@ -140,4 +99,10 @@ fun Greeting(viewModel: MainViewModel, context: Context) {
         }
     }
 }
+
+
+
+
+
+
 
--- app/src/main/java/com/example/filepersistencetest/MainViewModel.kt
+++ app/src/main/java/com/example/filepersistencetest/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.filepersistencetest
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.util.Log
+import androidx.core.content.contentValuesOf
 import androidx.core.content.edit
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
@@ -31,25 +33,11 @@ private val EXAMPLE_COUNTER = intPreferencesKey("example_counter")
 class MainViewModel @Inject constructor(
     @ApplicationContext private val context: Context
 ) : ViewModel() {
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "preference_settings")
-    //
-    val count: Flow<Int> = context.dataStore.data
-        .map { preferences ->
-            preferences[EXAMPLE_COUNTER] ?: 0
-        }
-
-    private val Context.protoDataStore: DataStore<Count> by dataStore(
-        fileName = "count.pb",
-        serializer = CountSerializer
-    )
-
-    val protoCount: Flow<Int> = context.protoDataStore.data
-        .map { count ->
-            count.exampleCounter
-        }
-
-
+    /*
+       file storage
+    */
     private var _text = MutableStateFlow("text")
+
     val text = _text.asStateFlow()
     fun setText(text: String) {
         _text.value = text
@@ -81,11 +69,13 @@ class MainViewModel @Inject constructor(
         return content.toString()
     }
 
+    /*
+    shared preference
+     */
     fun saveBySharedPreferences() =
         context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE).edit {
             putString("text", text.value)
         }
-
 
     fun loadBySharedPreferences() {
         val text =
@@ -93,14 +83,94 @@ class MainViewModel @Inject constructor(
         Log.d("getPreferenceValue", "text is $text")
     }
 
+    /*
+    SQLite
+     */
+    private val myDatabaseHelper = MyDatabaseHelper(context, "BookStore.db", 3)
+
+    private var db: SQLiteDatabase? = null
+    fun createDatabase() {
+        db = myDatabaseHelper.writableDatabase
+    }
+
+    fun insert() {
+        val values = contentValuesOf(
+            "name" to "The Da Vinci Code",
+            "author" to "Dan Brown",
+            "pages" to 454,
+            "price" to 16.96
+        )
+        db?.insert("Book", null, values)
+    }
+
+    fun delete(){
+        db?.delete("Book", "pages < ?", arrayOf("500"))
+    }
+
+    fun update() {
+        val values = contentValuesOf("price" to 10.99)
+        db?.update("Book", values, "name = ?", arrayOf("The Da Vinci Code"))
+    }
+
+    fun query() {
+        val cursor = db?.query("Book", null, null, null, null, null, null)
+        if (cursor?.moveToFirst() == true) {
+            do {
+                val name = cursor.getString(cursor.getColumnIndexOrThrow("name"))
+                Log.d("DataBase", "book name is $name")
+            } while (cursor.moveToNext())
+        }
+        cursor?.close()
+    }
+
+    fun transaction() {
+        db?.beginTransaction()
+        try {
+            db?.delete("Book", null, null)
+            val values = contentValuesOf(
+                "name" to "Game of Thrones",
+                "author" to "George Martin",
+                "pages" to 720,
+                "price" to 20.85
+            )
+            db?.insert("Book", null, values)
+            db?.setTransactionSuccessful()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            db?.endTransaction()
+        }
+    }
+
+    /*
+    Preferences DataStore
+     */
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "preference_settings")
+
+    val count: Flow<Int> = context.dataStore.data
+        .map { preferences ->
+            preferences[EXAMPLE_COUNTER] ?: 0
+        }
 
     suspend fun incrementCounter() {
-
         context.dataStore.edit { settings ->
             val currentCounterValue = settings[EXAMPLE_COUNTER] ?: 0
             settings[EXAMPLE_COUNTER] = currentCounterValue + 1
         }
     }
+
+    /*
+    Proto DataStore
+     */
+    private val Context.protoDataStore: DataStore<Count> by dataStore(
+        fileName = "count.pb",
+        serializer = CountSerializer
+    )
+
+    val protoCount: Flow<Int> = context.protoDataStore.data
+        .map { count ->
+            count.exampleCounter
+        }
 
     suspend fun incrementProtoCounter() {
         context.protoDataStore.updateData { currentSettings ->
